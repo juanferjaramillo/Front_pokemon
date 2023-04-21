80818553d6c9b4623945b0d32d4cc870ae9219a3
juanferjaramillo <juanfer.jaramillo@gmail.com>
2023-04-20 20:49:34 -0500

    built

--- src/index.js
+++ src/index.js
@@ -6,10 +6,10 @@ import reportWebVitals from "./reportWebVitals";
 import {Provider} from 'react-redux'
 import store from './redux/store'
 import {BrowserRouter} from 'react-router-dom';
-import axios from axios;
+import axios from "axios";
 
 // axios.defaults.baseURL = 'http://localhost:3001';
-axios.defaults.baseURL = 'https://backpokemon-production-6bdf.up.railway.app/';
+axios.defaults.baseURL = 'https://backpokemon-production-6bdf.up.railway.app';
 
 ReactDOM.render(
   <Provider store={store}>
